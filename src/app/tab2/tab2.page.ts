c13efe47ebd68292cfb83177f3dcfed659c44b79
AdrianGomezLuna <adrigoluna@gmail.com>
2021-09-15 11:07:38 +0200

    Diseño de búsqueda

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,13 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  textoBuscar = '';
+  ideas: string[] = ['Spiderman', 'Avenger', 'El señor de los anillos','La vida es bella'];
+
   constructor() {}
+
+  buscar( event) {
+    const valor = event.detail.value;
+  }
 
 }
