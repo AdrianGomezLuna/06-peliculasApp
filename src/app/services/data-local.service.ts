5e1536b3b6ef2ff6bf09e74d187dc0302088c5a7
AdrianGomezLuna <adrigoluna@gmail.com>
2021-09-15 13:13:04 +0200

    Prevenir duplicados

--- src/app/services/data-local.service.ts
+++ src/app/services/data-local.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 import { Storage } from '@ionic/storage-angular';
 import { PeliculaDetalle } from '../interfaces/interfaces';
 
@@ -10,7 +11,7 @@ export class DataLocalService {
   peliculas: PeliculaDetalle[] = [];
   private _storage: Storage | null = null;
 
-  constructor(private storage: Storage) {
+  constructor(private storage: Storage, private toastController: ToastController) {
     this.initDB();
   }
 
@@ -20,8 +21,35 @@ export class DataLocalService {
     this._storage = storage;
   }
 
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message ,
+      duration: 1500
+    });
+    toast.present();
+  }
+
   guardarPelicula(pelicula: PeliculaDetalle) {
-    this.peliculas.push(pelicula);
+    let existe = false;
+    let mensaje = '';
+    for(const peli of this.peliculas) {
+      if (peli.id === pelicula.id) {
+        existe = true;
+        break;
+      }
+    }
+
+    if (existe) {
+      this.peliculas = this.peliculas.filter( peli => peli.id !== pelicula.id );
+      mensaje ='Borrado de Favoritos';
+    } else {
+      this.peliculas.push(pelicula);
+      mensaje = 'Agregada a Favoritos';
+
+    }
+
+    this.presentToast(mensaje);
     this.storage.set('pelicula', this.peliculas);
   }
+
 }
